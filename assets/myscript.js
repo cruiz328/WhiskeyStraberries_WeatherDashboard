083ff436ccc8e20fa3bfdf63f472e9dab0ea2d6c
Christian Ruiz <lemmyruiz@Christians-MacBook-Pro.local>
2022-03-10 01:27:10 -0800

    updates js and html

--- assets/myscript.js
+++ assets/myscript.js
@@ -13,3 +13,24 @@ $.ajax({
   });
 
   //https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key}
+  const statesArr = [];
+
+  $('#search-state').on("click", function(e){
+    const state = $('#form1').val();
+    statesArr.push(state);
+    console.log(statesArr);
+    AddState();
+
+  })
+
+  function AddState() {
+    $('#display-states').empty()
+    for (var i = 0; i < statesArr.length; i++) {
+      const btnState = $('<button>');
+      btnState.html(statesArr[i])
+      btnState.addClass('showWeather');
+  
+      $('#display-states').append(btnState);
+    }
+  }
+ 
